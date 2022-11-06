e0031496689f4c86d3bc437a5bd2d8e1752ad232
Alina Akimova <m1905383@edu.misis.ru>
2022-11-06 19:48:27 +0300

    decode commit

--- zio-quickstart-restful-webservice/src/main/scala/dev/zio/quickstart/transaction/TransactionApp.scala
+++ zio-quickstart-restful-webservice/src/main/scala/dev/zio/quickstart/transaction/TransactionApp.scala
@@ -2,13 +2,37 @@ package dev.zio.quickstart.transaction
 
 import zhttp.http.*
 import zio.*
+import zio.json.*
 import zio.stream.ZStream
 import scala.io.Source
 
-object TransactionApp:
-  def apply(): Http[Any, Nothing, Request, Response] =
-    Http.collect[Request] {
-      case Method.GET -> !! / "transaction-check" =>
-        val scalaFileContents = Source.fromFile(file).getLines()
+case class Transaction(src: String,
+                       dst: String,
+                       amount: Int)
 
+object TransactionApp:
+  implicit val encoder: JsonEncoder[Transaction] = DeriveJsonEncoder.gen[Transaction]
+  implicit val decoder: JsonDecoder[Transaction] = DeriveJsonDecoder.gen[Transaction]
+  implicit val codec: JsonCodec[Transaction] = DeriveJsonCodec.gen[Transaction]
+
+  def apply(): Http[Any, Throwable, Request, Response] =
+    Http.collectZIO[Request] {
+      // POST /users -d '{"name": "John", "age": 35}'
+      case req@(Method.POST -> !! / "transaction-check") =>
+        val blacklist = Source.fromResource("bigfile.txt").getLines()
+        for {
+          u <- req.bodyAsString.map(_.fromJson[Transaction])
+          r <- u match
+            case Left(e) =>
+              ZIO.debug(s"Failed to parse the input: $e").as(
+                Response.text(e).setStatus(Status.BadRequest)
+              )
+            case Right(u) =>
+              for (line <- blacklist) {
+                if (line.equals(u.src) || line.equals(u.dst))
+                  Response.text("Cancel")
+                else
+                  Response.text("Succeed")
+              }
+        } yield r
     }
